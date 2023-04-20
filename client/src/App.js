a6b7f11de6d5c44dea62fa3c060c37a19b5c1646
shardul koli <monty@sharduls-MBP.fios-router.home>
2023-04-20 09:52:16 -0400

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { AdminLogin } from "./components/AdminComponents/AdminLogin";
 import { UserDetails } from "./components/UserDetails";
 
 function App() {
-	return (
-		<div className="App">
-			<UserDetails />
-		</div>
-	);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/userDetails" element={<UserDetails />} />
+          <Route path="/admin" element={<AdminLogin />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

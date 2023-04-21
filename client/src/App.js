f5f04bd04baae877d60095e0649965e82d813acd
Divyang <8656786+perseus01@users.noreply.github.com>
2023-04-21 18:14:49 -0400

    adding homepage component to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
-import { UserDetails } from "./components/UserDetails";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
+// import { UserDetails } from "./components/UserDetails";
+import { Homepage } from "./components/Homepage";
+import { NotFound } from "./components/NotFound";
 
 function App() {
 	return (
-		<div className="App">
-			<UserDetails />
-		</div>
+		<Router>
+			<div className="routes-body">
+				<Routes>
+					<Route path="/" element={<Homepage />} />
+					<Route path="/notfound" element={<NotFound />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 

4780132072df2a94491f6c13f44a2acbcf7c2e78
shardul koli <monty@sharduls-MacBook-Pro.local>
2023-05-08 22:36:03 -0400

    added image handling to admin dashboard

--- client/src/components/Reusables/ImageView.jsx
+++ client/src/components/Reusables/ImageView.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 export const ImageView = ({ imageSrc }) => {
-	return (
-		<div className="flex h-full justify-center bg-white bg-opacity-20">
-			<div className=" flex flex-col w-full md:py-8 my-auto justify-center items-center rounded">
-				<img src={imageSrc} alt="Receipt" />
-			</div>
-		</div>
-	);
+  console.log(imageSrc);
+  return (
+    <div className="flex h-full justify-center bg-white bg-opacity-20">
+      <div className=" flex flex-col w-full md:py-8 my-auto justify-center items-center rounded">
+        <img src={imageSrc} alt="Receipt" />
+      </div>
+    </div>
+  );
 };
